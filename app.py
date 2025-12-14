e9ef17cfe7d7a718c8569a8200fd030cbf3a7505
Sayan Mondal <sm7313617@gmail.com>
2025-12-14 16:49:38 +0530

    Configure Flask app for Render deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
